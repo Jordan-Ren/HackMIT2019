1d729749b18e165fcea7fe13ba2c44a72dccd8e0
Jordan Ren <51218151+Jordan-Ren@users.noreply.github.com>
2019-09-14 21:47:45 -0400

    some new stuff

--- Food Locator/dbCommunication/checklogin.py
+++ Food Locator/dbCommunication/checklogin.py
@@ -1,14 +1,17 @@
 import pyodbc
+import flask as Flask, request
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=hackmit2019.database.windows.net;DATABASE=HackMit2019;UID=texmix;PWD=Texm1x@2019')
-
-password = 'asdff'
-email ='jordan@jordan.com'
-cursor = conn.cursor()
-vars = {"E": email, "P": password}
-cursor.execute('''SELECT Email, Password FROM HackMit2019.dbo.Users''')
-for row in cursor.fetchall():
-    if password == row[1] and email == row[0]:
-        print('it match')
-else:
-    print('nah')
+app = Flask(__name__)
+@app.route('/checklogin')
+def checklogin():
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=hackmit2019.database.windows.net;DATABASE=HackMit2019;UID=texmix;PWD=Texm1x@2019')
+    password = 'asdff'
+    email ='jordan@jordan.com'
+    cursor = conn.cursor()
+    vars = {"E": email, "P": password}
+    cursor.execute('''SELECT Email, Password FROM HackMit2019.dbo.Users''')
+    for row in cursor.fetchall():
+        if password == row[1] and email == row[0]:
+            return 'True'
+    else:
+        return 'False'
