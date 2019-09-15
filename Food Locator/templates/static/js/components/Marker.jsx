ccaff19865c7318c2b5a43d2e4b74ea863b539e9
David He <ddavidhe127@gmail.com>
2019-09-15 06:04:15 -0400

    Switched indices

--- Food Locator/templates/static/js/components/Marker.jsx
+++ Food Locator/templates/static/js/components/Marker.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import style from './Marker.css';
 import axios from 'axios';
+import { red } from '@material-ui/core/colors';
 
 const Marker = (props) => {
-    const { color, name, id } = props;
-    let locationInfo  = '';
+    const {color, name, id } = props;
+    // let locationInfo = '';
+    
+    // function handleClick() {
+    //   alert(name)
+    // }
+  
+  //   // function renderColor() {
+  // //  { posted_data: 'example' })
+  //   async function getData() {
+  //     try {
+  //       return await axios.get('http://127.0.0.1:5000/list?location='+name)
+  //     } catch (e) {
+  //       console.log(`Axios request failed: ${e}`);
+  //     }
+  //   }
 
-    function renderColor() {
-      axios({
-        method: 'get',
-        url: 'http://127.0.0.1:5000/list?location='+name,
-      })
-        .then(function (response) {
-          console.log(response.data);
-          locationInfo = response;
-        });
-    }
+    // if (locationInfo.length > 0) {
+    //   let color = "green";
+    // } else {
+    //   let color = "red";
+    // }
+    // console.log(color);
+    // }
 
-    function handleClick() {
-      renderColor()
-      console.log("handleCLICK");
-    }
+    // function handleClick() {
+    //   renderColor()
+    // }
+    // console.log(name,locationInfo.response)
+    let markerColor = "red";
+    
     return (
       <div className={style.marker}
-        style={{ backgroundColor: color, cursor: 'pointer'}}
+        style={{ backgroundColor: markerColor, cursor: 'pointer'}}
         title={name}
-        onClick={handleClick()}
       />
     );
   };
