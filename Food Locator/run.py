cc8c08b459bca6a90bad857117eacc190985ad21
David He <ddavidhe127@gmail.com>
2019-09-14 23:23:38 -0400

    changed run file

--- Food Locator/run.py
+++ Food Locator/run.py
@@ -1,4 +1,13 @@
-from templates import app
+from flask import render_template, request, Flask
+import json
+
+app = Flask(__name__,
+ 	static_folder = './templates/public',
+ 	template_folder="./templates/static")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 #Load this config object for development mode
 app.config.from_object('configurations.DevelopmentConfig')
