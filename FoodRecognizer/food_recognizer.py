4eb62dff4604e64948ff44e4ae7ebca71162f59e
Alex Ellison <acelli@mit.edu>
2019-09-15 06:23:50 -0400

    Added in debugging

--- FoodRecognizer/food_recognizer.py
+++ FoodRecognizer/food_recognizer.py
@@ -4,16 +4,21 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
+print("Initialized Camera")
 
 camera.start_preview()
+print("Started Preview")
 sleep(5)
 camera.capture('/Images/current_food.jpg')
+print("Captured and saved photo to /Images/current_food.jpg")
 camera.stop_preview()
+print("Stopped Preview")
 
 visual_recognition = VisualRecognitionV3(
     version='v3',
     iam_apikey='1gkZwFC_6FexLtghypBX9gMAuTh0SzfmX3i6QOjyYHnq'
 )
+print("Authorized IBM Visual Recognizer")
 
 visual_recognition = VisualRecognitionV3(
 '2018-03-19',
@@ -30,6 +35,9 @@ print("Classes: ", classes)
 print(json.dumps(classes, indent=2))
 classifier = classes["images"][0]["classifiers"][0]["classes"][0]["class"]
 confidence = classes["images"][0]["classifiers"][0]["classes"][0]["score"]
+print(" ")
+print(" ")
+print("Result: ")
 if classifier == "non-food":
     print("We are " + str(confidence*100) + " percent sure that there is no food")
 else:
